95c376ba2490b9afa8cc3f38ed5e0c71d181b263
MaoXianxin <qq1044467857@gmail.com>
2019-03-05 20:22:19 +0800

    add more dataset

---
+++ process-csv-file.py
@@ -0,0 +1,31 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.3'
+#       jupytext_version: 0.8.6
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+import pandas as pd
+
+path = '~/Downloads/dataset/ChnSentiCorp_htl_all.csv'
+
+dataframe = pd.read_csv(path)
+
+type(dataframe['review'])
+
+new_path = './data/ChnSentiCorp_htl_all.txt'
+new_file = open(new_path, 'w')
+
+for line in dataframe['review']:
+    new_file.write(str(line))
+
+new_file.close()
+
+
